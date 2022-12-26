9f2adcf15005727fe6cfe19d6c59c159b7aff3b8
Thanos Sklavos <thanos.sklavos@outlook.com>
2022-12-26 17:51:06 +0200

    Version 0.2 added Holder class

--- cf3-testbed/bankapp/docs/type-search-index.js
+++ cf3-testbed/bankapp/docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"gr.aueb.cf.ch11.bankapp.model","l":"JointAccount"},{"p":"gr.aueb.cf.ch11.bankapp","l":"Main"},{"p":"gr.aueb.cf.ch11.bankapp.model","l":"OverdraftAccount"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"gr.aueb.cf.ch11.bankapp.model","l":"Holder"},{"p":"gr.aueb.cf.ch11.bankapp.model","l":"JointAccount"},{"p":"gr.aueb.cf.ch11.bankapp","l":"Main"},{"p":"gr.aueb.cf.ch11.bankapp.model","l":"OverdraftAccount"}]
